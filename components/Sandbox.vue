e8ec267e5dc5f36692a6db276cd157b4031d6124
Santiago <monokutho@gmail.com>
2022-05-01 16:46:32 +0200

    tone installed and connected

--- components/Sandbox.vue
+++ components/Sandbox.vue
@@ -1,14 +1,23 @@
 <template>
   <div>
     <h1>Sandbox</h1>
+    <button @click="playNote">C4</button>
   </div>
 </template>
 
 <script lang="ts">
   import Vue from 'vue'
+  import * as Tone from 'tone'
+
+  const synth = new Tone.Synth().toDestination();
 
   export default Vue.extend({
-    name: 'Sandbox'
+    name: 'Sandbox',
+    methods: {
+      playNote(): void {
+        synth.triggerAttackRelease("C4", "8n");
+      }
+    }
   })
 </script>
 
