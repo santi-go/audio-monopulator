55c3ad7133733349295d300f604b5fc3dbc9bd8b
Santiago <monokutho@gmail.com>
2022-05-01 16:36:34 +0200

    Initial Commit

---
+++ components/Sandbox.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>Sandbox</h1>
+  </div>
+</template>
+
+<script lang="ts">
+  import Vue from 'vue'
+
+  export default Vue.extend({
+    name: 'Sandbox'
+  })
+</script>
+
+<style scoped>
+
+</style>
